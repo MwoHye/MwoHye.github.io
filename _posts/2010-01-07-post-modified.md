5774e01f4c3989fa7d6d67be4f497f5f28735c5f
MwoHye <eunsunnou@gmail.com>
2021-10-22 10:26:56 +0900

    다크모드, 폰트 조선명조체 변경

---
+++ _posts/2010-01-07-post-modified.md
@@ -0,0 +1,14 @@
+---
+title: "Post: Modified Date2"
+categories:
+  - Post Formats
+tags:
+  - Post Formats
+  - readability
+  - standard
+last_modified_at: 2021-10-22T13:01:27-05:00
+---
+
+This post has been updated and should show a modified date if `last_modified_at` is used in the layout.
+
+Plugins like [**jekyll-sitemap**](https://github.com/jekyll/jekyll-feed) use this field to add a `<lastmod>` tag your `sitemap.xml`.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Post: Modified Date2",
+        "excerpt":"This post has been updated and should show a modified date if last_modified_at is used in the layout.   Plugins like jekyll-sitemap use this field to add a &lt;lastmod&gt; tag your sitemap.xml.  ","categories": ["Post Formats"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/post%20formats/post-modified/",
+        "teaser": null
+      }]
