1b6f8aebe8440e0a7be93fda03af9e4482ade83b
MwoHye <eunsunnou@gmail.com>
2021-11-06 16:17:31 +0900

    SQLD 문제집 풀이 43번까지

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "SQL 정리(2/5) - 2과목 1장, SQL 기본",
-        "excerpt":"제 1절. 관계형 데이터베이스 개요 데이터베이스 : “특정 기업이나 조직 또는 개인이 필요에 의해 데이터를 일정한 형태로 저장한 것” SQL : “관계형데이터베이스에서 데이터 정의 데이터 조작, 데이터 제어를 하기 위해 사용하는 언어” 데이터 조작어 (DML) * M : Manipulation DB 내 데이터에 대한 조회, 검색, 변형을 위한 명령어 SELECT, INSERT,...","categories": ["SQL"],
+        "excerpt":"  제 1절. 관계형 데이터베이스 개요 데이터베이스 “특정 기업이나 조직 또는 개인이 필요에 의해 데이터를 일정한 형태로 저장한 것” SQL “관계형데이터베이스에서 데이터 정의 데이터 조작, 데이터 제어를 하기 위해 사용하는 언어” 데이터 조작어 (DML) : SELECT, INSERT, DELETE, UPDATE DB 내 데이터에 대한 조회, 검색, 변형을 위한 명령어 (M :...","categories": ["SQL"],
         "tags": ["SQL","SQLD","자격증"],
         "url": "/sql/SQLD-2/",
         "teaser": null
@@ -15,5 +15,17 @@ var store = [{
         "excerpt":"FOREIGN KEY에 의한 ACTIONS ON DELETE 부모 테이블의 행이 삭제될 때 자식 테이블에 일어나는 일들 CASDADE : 부모 삭제 시 자식도 삭제 SET NULL : 부모 삭제 시 자식은 NULL로 설정 SET DEFAULT : 부모 삭제 시 자식은 기본값 설정 RESTRICT : 자식이 없는 경우만 부모 삭제 ON INSERT 자식...","categories": ["SQL"],
         "tags": ["SQL","SQLD","자격증"],
         "url": "/sql/SQLD-3/",
+        "teaser": null
+      },{
+        "title": "SQL 정리(4/5) - 2과목 2장, SQL 활용",
+        "excerpt":"   제 1절. 표쥰 조인   제 2절. 집합 연산자   제 3절. 계층형 질의와 셀프 조인   제 4절. 서브쿼리   제 5절. 그룹 함수   제 6절. 윈도우 함수   제 7절. DCL   제 8절. 절차형 SQL  ","categories": ["SQL"],
+        "tags": ["SQL","SQLD","자격증"],
+        "url": "/sql/SQLD-4/",
+        "teaser": null
+      },{
+        "title": "SQL 정리(5/5) - 2과목 3장, SQL 최적화 기본 원리",
+        "excerpt":"   제 1절. 옵티마이저와 실행계획   제 2절. 인덱스 기본   제 3절. 조인 수행 원리  ","categories": ["SQL"],
+        "tags": ["SQL","SQLD","자격증"],
+        "url": "/sql/SQLD-5/",
         "teaser": null
       }]
