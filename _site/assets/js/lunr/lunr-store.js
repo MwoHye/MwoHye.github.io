c24358b54e8bf8eb13ec31bad4709f13f2b53e1e
MwoHye <eunsunnou@gmail.com>
2021-11-16 17:26:32 +0900

    GROUP BY절

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,38 +1,44 @@
 var store = [{
-        "title": "SQL 정리(1/5) - 1과목, 데이터 모델링의 이해 및 데이터 모델과 성능",
+        "title": "SQLD 정리(1/4) - 1과목, 데이터 모델링의 이해 및 데이터 모델과 성능",
         "excerpt":"  제 1절. 데이터모델의 이해 1. 모델링의 이해 모델링 : “다양한 현상을 표기법에 의해 표기하는 것” 특징 3가지 : 추상화, 단순화, 명확화 관점 3가지 : 데이터 관점, 프로세스 관점, 상관 관점 2. 데이터모델의 기본개념이해 데이터모델링 : “정보시스템을 구축하기 위한 데이터관점의 업무분석기법” 데이터모델이 제공하는 기능 시스템 가시화, 시스템 구조와 행동 명세화,...","categories": ["SQL"],
         "tags": ["SQL","SQLD","자격증"],
         "url": "/sql/SQLD-1/",
         "teaser": null
       },{
-        "title": "SQL 정리(2/5) - 2과목 1장, SQL 기본",
+        "title": "SQL - FOREIGN KEY에 의한 명령어 정리 (CASCADE, SET NULL)",
+        "excerpt":"FOREIGN KEY에 의한 ACTIONS ON DELETE 부모 테이블의 행이 삭제될 때 자식 테이블에 일어나는 일들 CASDADE : 부모 삭제 시 자식도 삭제 SET NULL : 부모 삭제 시 자식은 NULL로 설정 SET DEFAULT : 부모 삭제 시 자식은 기본값 설정 RESTRICT : 자식이 없는 경우만 부모 삭제 ON INSERT 자식...","categories": ["SQL"],
+        "tags": ["SQL","SQLD","자격증"],
+        "url": "/sql/SQLD-11/",
+        "teaser": null
+      },{
+        "title": "SQLD 정리(2/4) - 2과목 1장, SQL 기본",
         "excerpt":"  제 1절. 관계형 데이터베이스 개요 데이터베이스 “특정 기업이나 조직 또는 개인이 필요에 의해 데이터를 일정한 형태로 저장한 것” SQL “관계형데이터베이스에서 데이터 정의 데이터 조작, 데이터 제어를 하기 위해 사용하는 언어” 1. 데이터 조작어 (DML) : SELECT, INSERT, DELETE, UPDATE, MERGE DB 내 데이터에 대한 조회, 검색, 변형을 위한 명령어...","categories": ["SQL"],
         "tags": ["SQL","SQLD","자격증"],
         "url": "/sql/SQLD-2/",
         "teaser": null
       },{
-        "title": "SQL - FOREIGN KEY에 의한 명령어 정리 (CASCADE, SET NULL)",
-        "excerpt":"FOREIGN KEY에 의한 ACTIONS ON DELETE 부모 테이블의 행이 삭제될 때 자식 테이블에 일어나는 일들 CASDADE : 부모 삭제 시 자식도 삭제 SET NULL : 부모 삭제 시 자식은 NULL로 설정 SET DEFAULT : 부모 삭제 시 자식은 기본값 설정 RESTRICT : 자식이 없는 경우만 부모 삭제 ON INSERT 자식...","categories": ["SQL"],
+        "title": "SQL 정리(3/4) - 2과목 2장, SQL 활용",
+        "excerpt":"  제 1절. 표준 조인 STANDARD SQL 일반 집합 연산자 UNION -&gt; UNION INTERSECTION -&gt; INTERSECT DIFFERENCE -&gt; EXCEPT PRODUCT -&gt; CROSS JOIN (CARTESIAN PRODUCT) 순수관계연산자 SELECT -&gt; WHERE (행 선택) PROJECT -&gt; SELECT (열 선택) (NATURAL) JOIN -&gt; 다양한 JOIN DIVIDE -&gt; 사용 X 제 2절. 집합 연산자 제...","categories": ["SQL"],
         "tags": ["SQL","SQLD","자격증"],
         "url": "/sql/SQLD-3/",
         "teaser": null
       },{
-        "title": "SQL 정리(4/5) - 2과목 2장, SQL 활용",
-        "excerpt":"  제 1절. 표준 조인 STANDARD SQL 일반 집합 연산자 UNION -&gt; UNION INTERSECTION -&gt; INTERSECT DIFFERENCE -&gt; EXCEPT PRODUCT -&gt; CROSS JOIN (CARTESIAN PRODUCT) 순수관계연산자 SELECT -&gt; WHERE (행 선택) PROJECT -&gt; SELECT (열 선택) (NATURAL) JOIN -&gt; 다양한 JOIN DIVIDE -&gt; 사용 X 제 2절. 집합 연산자 제...","categories": ["SQL"],
+        "title": "SQL 정리(4/4) - 2과목 3장, SQL 최적화 기본 원리",
+        "excerpt":"   제 1절. 옵티마이저와 실행계획   제 2절. 인덱스 기본   제 3절. 조인 수행 원리  ","categories": ["SQL"],
         "tags": ["SQL","SQLD","자격증"],
         "url": "/sql/SQLD-4/",
         "teaser": null
       },{
-        "title": "SQL 정리(5/5) - 2과목 3장, SQL 최적화 기본 원리",
-        "excerpt":"   제 1절. 옵티마이저와 실행계획   제 2절. 인덱스 기본   제 3절. 조인 수행 원리  ","categories": ["SQL"],
+        "title": "SQLD 핵심노트 (1/2)",
+        "excerpt":"해당 내용은 김강민 SQLD ‘SQLD 최종 정리강의 1편’과 관련된 내용입니다. 관련 영상 링크 : https://www.youtube.com/watch?v=PC3ypt_VGWI&amp;t 1. SQL 명령문 개괄 ★ SQL 연산 순서 SELECT – 5 FROM – 1 WHERE – 2 GROUP BY – 3 HAVING – 4 ORDER BY – 6 DML / DDL / DCL / TCL...","categories": ["SQL"],
         "tags": ["SQL","SQLD","자격증"],
-        "url": "/sql/SQLD-5/",
+        "url": "/sql/SQLD-91/",
         "teaser": null
       },{
         "title": "SQLD 핵심노트 (1/2)",
         "excerpt":"  SQL 명령문 개괄 ★ SQL 연산 순서 SELECT – 5 FROM – 1 WHERE – 2 GROUP BY – 3 HAVING – 4 ORDER BY – 6 DML SELECT, DELETE, UPDATE, INSERT DDL CREATE, DROP, ALTER, RENAME, TRUNCATE DCL REVOKE, GRANT TCL COMMIT, ROLLBACK, SAVEPOINT   SELECT절 alias SELECT...","categories": ["SQL"],
         "tags": ["SQL","SQLD","자격증"],
-        "url": "/sql/SQLD-6/",
+        "url": "/sql/SQLD-92/",
         "teaser": null
       },{
         "title": "[SQLD 최종오답노트] 과목 1 - 데이터의 이해",
